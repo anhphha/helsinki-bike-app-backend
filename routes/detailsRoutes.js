89512d7368a1061fcbb99bd95845298862508f65
anhphha <74095848+anhphha@users.noreply.github.com>
2023-03-18 00:55:29 +0200

    deleted routes and controllers folder

--- routes/detailsRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const detailsRouter = express.Router();
-const detailsController = require("../controllers/detailsController");
-
-detailsRouter.route("/").get(detailsController.getDetails);
-
-module.exports = detailsRouter;
--- routes/searchRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const searchRouter = express.Router();
-const searchController = require("../controllers/searchController");
-
-searchRouter.route("/").get(searchController.getSearched);
-
-module.exports = searchRouter;
